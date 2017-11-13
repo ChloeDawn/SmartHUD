b1b6868f28754fddd652abdce3354d1e940ef65c
InsomniaKitten <admin@designdevils.co.uk>
2017-11-13 13:02:47 +0000

    Collect baubles

--- src/main/java/net/insomniakitten/smarthud/compat/baubles/BaubleSlotCache.java
+++ src/main/java/net/insomniakitten/smarthud/compat/baubles/BaubleSlotCache.java
@@ -16,9 +16,15 @@ package net.insomniakitten.smarthud.compat.baubles;
  *   limitations under the License.
  */
 
+import baubles.api.BaublesApi;
+import baubles.api.cap.IBaublesItemHandler;
 import net.insomniakitten.smarthud.SmartHUD;
+import net.insomniakitten.smarthud.feature.hotbar.InventoryCache;
+import net.insomniakitten.smarthud.util.CachedItem;
 import net.insomniakitten.smarthud.util.ModProfiler;
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -27,13 +33,26 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod.EventBusSubscriber(modid = SmartHUD.ID, value = Side.CLIENT)
 public final class BaubleSlotCache {
 
+    private static NonNullList<CachedItem> baubles = NonNullList.create();
+
     private BaubleSlotCache() {}
+
+    public static NonNullList<CachedItem> getBaubles() {
+        return baubles;
+    }
 
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event) {
         Minecraft mc = Minecraft.getMinecraft();
         if (mc.player == null || mc.player.world == null) return;
         ModProfiler.start(ModProfiler.Section.CACHE_BAUBLES);
+        IBaublesItemHandler handler = BaublesApi.getBaublesHandler(mc.player);
+        NonNullList<CachedItem> cache = NonNullList.create();
+        for (int slot = 0; slot < handler.getSlots(); ++slot) {
+            ItemStack bauble = handler.getStackInSlot(slot);
+            InventoryCache.processItemStack(cache, bauble);
+        }
+        baubles = cache;
         ModProfiler.end();
     }
 
